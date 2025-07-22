17f7d14ecd9496a5577f291c5ce3277f8577aaf7
NIDA MUZAFFAR <nidamuzaffar606@gmail.com>
2025-07-22 23:51:20 +0500

    new chsnge

---
+++ homework.py
@@ -0,0 +1,79 @@
+todo_list = [{"name": "example", "completed": False},
+             {"name": "example2", "completed": True}
+            ]
+
+print(todo_list)
+
+def display_task(any_list):
+    count = 1
+    for task in any_list:
+        print(f"{count}) {task["name"]}: {task["completed"]}")
+        count += 1
+
+display_task(todo_list)
+
+type(bool(int(0))), bool(int('1'))
+
+def add_task(any_list):
+    name = input("Enter task name:")
+    task = {"name": name, "completed": False}
+
+    any_list.append(task)
+    return any_list
+
+todo_list = add_task(todo_list)
+
+display_task(todo_list)
+
+def complete_task(any_list):
+    print("Look your list and enter task number to complete it")
+    display_task(any_list)
+
+    task_number = int(input("Enter number here:"))
+    task = any_list[task_number-1]
+    task["completed"] = True
+
+    return any_list
+
+todo_list = complete_task(todo_list)
+
+display_task(todo_list)
+
+def delete_task(any_list):
+    print("Look your list and enter task number to delete it")
+    display_task(any_list)
+
+    task_number = int(input("Enter number here:"))
+    task = any_list[task_number-1]
+    todo_list.remove()
+
+    return any_list
+
+todo_list = delete_task(todo_list)
+
+display_task(todo_list)
+
+def run_todo():
+    todo_list = []
+    while True:
+        print("Welcome to todo list")
+        print("Option 1) Display Task")
+        print("Option 2) Add Task")
+        print("Option 3) Complete Task")
+        print("Option 4) delete Task")
+        print("Option 5) Exit")
+        
+        option = input("\n Enetr any option \n")
+        if option == "1":
+            display_task(todo_list)
+        elif option == "2":
+            todo_list = add_task(todo_list)
+        elif option == "3":
+            todo_list = complete_task(todo_list)    
+        elif option == "4":
+            print("Thankyou")
+            break
+        else:
+            print("Invalid Option: please enter correct option")
+ 
+run_todo()
